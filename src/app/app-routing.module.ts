445f01c763c90138f90fb154ab0d444b4390ef4d
Nathan Easton <nathan.easton@avanade.com>
2019-02-27 15:17:56 -0700

    Routing logic day 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { CourseComponent } from './course/course.component';
+import { LoginComponent } from './user/login.component';
 
-const routes: Routes = [];
+
+const ROUTES: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  /* redefine path below */
+  { 
+    path: '**', component: HomeComponent
+  },
+  /*
+  {
+    path: 'course', component: CourseComponent
+  },
+  {
+    path: 'user', component: UserComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  */
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { CourseComponent } from './course/course.component';
+import { CourseChapterComponent } from './course/course-chapter/course-chapter.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent,
+    CourseComponent,
+    CourseChapterComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
