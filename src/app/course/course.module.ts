fc82a3d4606e4816864bd7c2cea04350d080506e
Zachary T. Kegel <zachary.t.kegel@avanade.com>
2019-02-28 09:52:14 -0700

    started course chapter component. Added routing from course 'list' page

--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseComponent } from '../course/course.component';
 import { RouterModule } from '@angular/router';
+import { CourseChapterComponent } from './course-chapter/course-chapter.component';
 
 @NgModule({
   declarations:
   [
-    CourseComponent
+    CourseComponent,
+    CourseChapterComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: 'course', component: CourseComponent }
+      { path: 'course', component: CourseComponent },
+      { path: 'course/:id', component: CourseChapterComponent}
     ])
   ]
 })
