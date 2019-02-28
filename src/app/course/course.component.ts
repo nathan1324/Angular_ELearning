37dea144ec7bf9cb4ce12b738fa78d9ef0cdd2fd
Zachary T. Kegel <zachary.t.kegel@avanade.com>
2019-02-28 09:26:22 -0700

    Added course dummy data and begain creating course list

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   templateUrl: './course.component.html',
@@ -6,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class CourseComponent {
   pageTitle = 'Course Catalog';
+
+  courses: Course[] = [
+    {
+      id: 1,
+      courseName: 'Course 1',
+      courseCode: 'C101',
+      description: 'This is an intro level course'
+    },
+    {
+      id: 2,
+      courseName: 'Course 2',
+      courseCode: 'C201',
+      description: 'This is an intermediate level course'
+    },
+    {
+      id: 3,
+      courseName: 'Course 3',
+      courseCode: 'C301',
+      description: 'This is an advanced level course'
+    },
+    {
+      id: 4,
+      courseName: 'Course 1',
+      courseCode: 'C401',
+      description: 'This is an expert level course'
+    }
+  ];
 }
